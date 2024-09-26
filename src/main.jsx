01e1977c0ec4ee2f4ce76076dd4f4e4226cd63cd
Sophanarath <www.sophanarath77@gmail.com>
2024-09-26 11:07:36 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -89,7 +89,6 @@ const router = createBrowserRouter([
 ]);
 const helmetContext = {};
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider context={helmetContext}>
